49eeb2f6c3707f684041f0b272d0365edeb82037
Chris Vik <maxvram@chrisvik.com>
2025-08-04 09:23:40 +1000

    Added proper commenting to scripts

--- script_flattener.py
+++ script_flattener.py
@@ -28,14 +28,16 @@ def flatten_directory(details, src_dir, dest_dir) -> int:
     detail_comment = ""
 
     for root, _, files in os.walk(src_dir):
+        relative_folder = os.path.relpath(root, src_dir)
         for file in files:
             if details and file.endswith(".asmdef"):
-                # Path to the asmdef file, relative to the source directory
-                asmdef_path = os.path.join(root, file)
+                # Path to the asmdef file relative to the source directory (not including the source directory itself)
+                asmdef_path = os.path.join(relative_folder, file)
                 print(f"Found asmdef: {asmdef_path}")
                 break
 
-        detail_comment = f"// Asmdef: {asmdef_path}\n// Path: {os.path.relpath(root, src_dir)}\n"
+        detail_comment = f"// Asmdef: {asmdef_path}\n// Path: {relative_folder}\n\n"
+
         for file in files:
             if file.endswith(".cs"):
                 src_file_path = os.path.join(root, file)
@@ -54,7 +56,7 @@ def flatten_directory(details, src_dir, dest_dir) -> int:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flatten directory structure by copying .CS files.")
-    parser.add_argument("--details", type=bool, default=False, help="Comment .CS files with asmdef name and path.")
+    parser.add_argument("--details", type=bool, default=True, help="Comment .CS files with asmdef name and path.")
     parser.add_argument("--src_dir", type=str, default=SRC_DIR, help="Source directory containing .CS files.")
     parser.add_argument("--dest_dir", type=str, default=DEST_DIR, help="Destination directory for flattened files.")
 
